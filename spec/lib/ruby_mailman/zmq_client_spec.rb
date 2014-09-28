a4fca497c02d76ba425e9ef26c04fe32d4618444
Ian Whitney <whit0694@umn.edu>
2014-09-28 12:56:44 -0500

    Test ZMQRequestClient
    This test is a little odd. The previous implementation mocked everything
    out and ensured that the ZMQ stuff was being set up correctly. But
    that's all implementation and shouldn't be tested.
    
    The only way to really test this is to have it talk to a real ZMQ server
    that can handle REQ/REP. So this testing approach introduces a simple
    REQ/REP server in `spec/zmq_servers/reply` that can be spun up before a
    test and then killed afterwards.
    
    This testing server can also be configured so that we can randomize
    replies or change ports.
    
    The resulting test in more integration-ish, since it talks to an
    external service. But I'm keeping it in unit tests because that external
    service is as stub-like as I can make it. The real integration tests
    will test end-to-end: from the customer-facing API all the way down to a
    ZMQServer instance.

--- spec/lib/ruby_mailman/zmq_client_spec.rb
+++ spec/lib/ruby_mailman/zmq_client_spec.rb
@@ -1,5 +1,6 @@
 require "spec_helper"
 require_relative "../../../lib/ruby_mailman"
+require_relative "../../zmq_servers/reply"
 
 RSpec.describe ZMQClient do
   describe "request" do
@@ -24,3 +25,28 @@ RSpec.describe ZMQClient do
     end
   end
 end
+
+RSpec.describe ZMQRequestClient do
+  describe "run" do
+    before(:example) do
+      reply_randomizer = rand
+      @server_configuration = {
+        port: 6666,
+        reply: -> {"reply #{reply_randomizer}"}
+      }
+      @pid = TestServer::Reply.run_as_process(@server_configuration)
+    end
+
+    it "returns a reply" do
+      action = 'action'
+      object = 'object'
+      reply = ZMQRequestClient.run(action,object)
+      expect(reply).to eq(@server_configuration[:reply].call)
+    end
+
+    after(:example) do
+      Process.kill("KILL", @pid)
+    end
+  end
+end
+
